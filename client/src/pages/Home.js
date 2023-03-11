12bd04c28f322d2ef27d44874c995ba16394b8ff
Chidananda Sahu <chidananda@sugarcosmetics.com>
2023-03-11 22:44:53 +0530

    pages and products add

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import Products from '../components/Products'
+
+const Home = () => {
+
+
+  return (
+    <div>
+        <h2 className='heading'>Welcome to the Redux Toolkit Store</h2>
+        <section>
+            <h3>Products</h3>
+            <Products/>
+        </section>
+    </div>
+  )
+}
+
+export default Home
